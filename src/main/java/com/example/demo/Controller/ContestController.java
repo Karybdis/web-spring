3ba8adfd2a80a92141779da41b0f1200478d7944
sunsc <584156664@qq.com>
2017-12-10 15:56:34 +0800

    edit match

--- src/main/java/com/example/demo/Controller/ContestController.java
+++ src/main/java/com/example/demo/Controller/ContestController.java
@@ -8,22 +8,25 @@ import com.example.demo.Repository.ContestTagRepository;
 import com.example.demo.Repository.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 @Controller
 public class ContestController
 {
     @Autowired
-    private ContestRepository contestRepository;
+    private ContestRepository contestRepository;   //比赛
     @Autowired
-    private TagRepository tagRepository;
+    private TagRepository tagRepository;           //标签
     @Autowired
-    private ContestTagRepository contestTagRepository;
+    private ContestTagRepository contestTagRepository;   //比赛标签联系
 
     @PostMapping("/addcontest")  //添加比赛
     public  String  addcontest(@RequestParam String contestName, @RequestParam String information, @RequestParam ArrayList<String> tags, HttpSession session)
@@ -38,10 +41,10 @@ public class ContestController
             {
                 Tag tag=new Tag();
                 ContestTag contestTag=new ContestTag();
-                contestTag.setContestId(contestRepository.findByContestName(contestName).getId());
+                contestTag.setContestId(contest.getId());
                 tag.setTagName(tagName);
                 tagRepository.save(tag);
-                contestTag.setTagId(tagRepository.findByTagName(tagName).getId());
+                contestTag.setTagId(tag.getId());
                 contestTagRepository.save(contestTag);
             }
         }
@@ -55,6 +58,23 @@ public class ContestController
         {
             if (contestRepository.findOne(id) != null)    //该ID的比赛存在则删除
                 contestRepository.delete(id);
+        }
+        return "redirect:/home";
+    }
+
+    @GetMapping("/editcontest")
+    public String editcontest(@RequestParam Long id, HttpSession session, Model model)
+    {
+        if ((int)session.getAttribute("admin")==1)  //管理员判定
+        {
+            ArrayList<ContestTag> contestTaglist=contestTagRepository.findByContestId(id);
+            ArrayList<Tag> taglist=new ArrayList<>();
+            for (ContestTag contestTag:contestTaglist)
+                taglist.add(tagRepository.findOne(contestTag.getTagId()));
+            Contest contest=contestRepository.findOne(id);
+            model.addAttribute("contest",contest);
+            model.addAttribute("taglist",taglist);
+            return "edit_contest";
         }
         return "redirect:/home";
     }
--- src/main/java/com/example/demo/Repository/ContestTagRepository.java
+++ src/main/java/com/example/demo/Repository/ContestTagRepository.java
@@ -3,6 +3,9 @@ package com.example.demo.Repository;
 import com.example.demo.Entity.ContestTag;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ContestTagRepository extends CrudRepository<ContestTag,Long>
 {
+    ArrayList<ContestTag> findByContestId(Long id);
 }
